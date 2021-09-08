46dab6f91ae9b80670b495a40f51481e93d0c1fd
insell824 <public@insell.link>
2021-09-08 12:45:25 +0900

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM docker.io/library/node@sha256:289a5e686ccfd79d2cf6e3cded8a5baae90eeb655e46c9c9213ac9dcd67ce3a0
 
+ARG APP_TYPE
+ARG APP_PORT
+ENV APP_TYPE=$APP_TYPE
+
 WORKDIR /usr/src/app
 
 
@@ -8,6 +12,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 
-EXPOSE 3000
+EXPOSE $APP_PORT
 
 CMD [ "node", "./bin/www" ]
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const APP_TYPE = process.env.APP_TYPE || 'NO_APP_TYPE'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: APP_TYPE });
 });
 
 module.exports = router;
