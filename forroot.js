7e50c2634fa73f2e990a082d80b73fa6102eddfb
insell824 <public@insell.link>
2021-09-08 17:14:58 +0900

    Update forroot.js

--- forroot.js
+++ forroot.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 var logger = require('morgan')
 app.use(logger('tiny'))
 app.get('/**', (req, res) => {
-  res.send(`SSH EC2 から 起動 path:${req.url}`)
+  res.send(`Message: SSH EC2 から 起動 <br> Path: ${req.url} <br> Time: ${Date.now() / 1000}`)
 })
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at ${port}`)
